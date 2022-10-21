31416caa2a11f0d23c8833e50fd76b8a0ecc2464
jeffmikhail <mikhailcruz78@gmail.com>
2022-10-21 09:57:19 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+const PORT = 3000;
+
+// * ROUTES
+app.get('/hello', (req, res) => {
+	res.send(`Task Manager App!`);
+});
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
