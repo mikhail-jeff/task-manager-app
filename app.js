c6095141b0f683701a5228d48b9e1ddec0c5ee98
jeffmikhail <mikhailcruz78@gmail.com>
2022-10-21 11:13:47 +0800

    updated route structure and details

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const app = express();
+const routes = require('./routes/tasks');
 
-const PORT = 3000;
+const PORT = 3001;
 
-// * ROUTES
-app.get('/hello', (req, res) => {
+// * middleware
+app.use(express.json());
+
+// * routes
+app.get('/', (req, res) => {
 	res.send(`Task Manager App!`);
 });
+
+app.use('/api/v1/tasks', routes);
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
